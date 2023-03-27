cf48ce631ac433e9ca4c22d9b769823ef092ef46
nirobhasan-git <papon.nirob@gmail.com>
2023-03-27 06:53:33 +0600

    Login Page Update

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -11,13 +11,20 @@ const Login = () => {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // const [users, setUsers] = useState('');
 
   const handleLogin = () => {
     console.log(`Email: ${email}, Password: ${password}`, `Users: ${users}`);
     // Add logic to handle login
-
+    if (email === 'gaddafy@gmail.com' && password === 'gaddafy') {
+      navigation.navigate('Faculty_dashboard', { email, password });
+    } else if (email === 'deori@gmail.com' && password === 'deori') {
+      navigation.navigate('Student_dashboard', { email, password });
+    } else {
+      navigation.navigate('Dashboard', { email: email, password: password, users: users });
+    }
     // Navigate to the Profile screen with the email and password values as params
-    navigation.navigate('Dashboard', { email: email, password: password, users: users });
+    // navigation.navigate('Dashboard', { email: email, password: password, users: users });
   };
 
   return (
